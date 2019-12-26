2ba87bb3bfc2376c281d0340ee7d786f22e9aa03
Joe Fair <joe@fairanswers.com>
2019-12-26 14:25:04 -0700

    Got k8s client working with example values.  Now need to get actual SAs.

--- src/main/java/com/fairanswers/KubeUser/UserRepository.java
+++ src/main/java/com/fairanswers/KubeUser/UserRepository.java
@@ -1,20 +1,68 @@
 package com.fairanswers.KubeUser;
 
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.ClientBuilder;
+import io.kubernetes.client.util.KubeConfig;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * A simple example of how to use the Java API from an application outside a kubernetes cluster
+ *
+ * <p>Easiest way to run this: mvn exec:java
+ * -Dexec.mainClass="io.kubernetes.client.examples.KubeConfigFileClientExample"
+ *
+ * <p>From inside $REPO_DIR/examples
+ */
+
+
+
 public class UserRepository {
 
-	User[] allUsers =new User[] {new User("1", "1","1", "1"), new User("2", "2","2", "2")};
-	public User[] list() {
-		return allUsers;
+	public boolean connect() {
+		
+		return true;
 	}
 
-	public User[] read(String id) {
-		if(id.equals("1") ) {
-			return new User[] {allUsers[0]};
+	public User[] list() {
+		String kubeConfigPath = "kubeconfig.yaml";
+		User [] users = new User[100];
+
+		try {
+			// loading the out-of-cluster config, a kubeconfig from file-system
+			ApiClient client = ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader(kubeConfigPath))).build();
+	
+			// set the global default api-client to the in-cluster one from above
+			Configuration.setDefaultApiClient(client);
+	
+			// the CoreV1Api loads default api-client from global configuration.
+			CoreV1Api api = new CoreV1Api();
+	
+			int i=0;
+			// invokes the CoreV1Api client
+			V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+			for (V1Pod item : list.getItems()) {
+				users[i]=new User(item.toString() , "", "", "");
+				if(i==99) {
+					break;
+				}
+			}
 		}
-		if(id.equals("2") ) {
-			return new User[] {allUsers[1]};
+		catch(Exception ex) {
+			System.out.println("Exception ="+ex);
 		}
-		return new User[] {};
+		return users;
+	}
+
+	public User[] read(String string) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
