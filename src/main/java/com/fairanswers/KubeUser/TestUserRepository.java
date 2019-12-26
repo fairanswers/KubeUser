52bdf47958f6685cfab9b6f13acd84d0e527bf85
Joe Fair <joe@fairanswers.com>
2019-12-26 14:25:58 -0700

    Catchup with kubeconfig and TestUserRepo.

---
+++ src/main/java/com/fairanswers/KubeUser/TestUserRepository.java
@@ -0,0 +1,20 @@
+package com.fairanswers.KubeUser;
+
+public class TestUserRepository {
+
+	User[] allUsers =new User[] {new User("1", "1","1", "1"), new User("2", "2","2", "2")};
+	public User[] list() {
+		return allUsers;
+	}
+
+	public User[] read(String id) {
+		if(id.equals("1") ) {
+			return new User[] {allUsers[0]};
+		}
+		if(id.equals("2") ) {
+			return new User[] {allUsers[1]};
+		}
+		return new User[] {};
+	}
+
+}
