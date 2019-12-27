7792ff388cbeed36018453501a42e2864c4527cc
Joe Fair <joe@fairanswers.com>
2019-12-27 05:00:33 -0700

    LIst now retrieves ca nad token.  Next need to generate kubeconfig.

--- src/main/java/com/fairanswers/KubeUser/UserRepository.java
+++ src/main/java/com/fairanswers/KubeUser/UserRepository.java
@@ -6,6 +6,9 @@ import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.openapi.models.V1Pod;
 import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.openapi.models.V1Secret;
+import io.kubernetes.client.openapi.models.V1ServiceAccount;
+import io.kubernetes.client.openapi.models.V1ServiceAccountList;
 import io.kubernetes.client.util.ClientBuilder;
 import io.kubernetes.client.util.KubeConfig;
 import java.io.FileReader;
@@ -24,6 +27,8 @@ import java.util.ArrayList;
 
 
 public class UserRepository {
+
+	private static final Integer TIMEOUT_SEC = 60;
 
 	public boolean connect() {
 		
@@ -46,18 +51,50 @@ public class UserRepository {
 	
 			int i=0;
 			// invokes the CoreV1Api client
-			V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
-			for (V1Pod item : list.getItems()) {
-				users[i]=new User(item.toString() , "", "", "");
+			//V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+			V1ServiceAccountList list = api.listServiceAccountForAllNamespaces(false, null, null, null, 1000000, null, null, TIMEOUT_SEC, false);
+			for (V1ServiceAccount item : list.getItems()) {
+				users[i]=getUserFromServiceAccount(api, item);
 				if(i==99) {
 					break;
 				}
+				i++;
 			}
 		}
 		catch(Exception ex) {
 			System.out.println("Exception ="+ex);
 		}
 		return users;
+	}
+
+	private User getUserFromServiceAccount(CoreV1Api api, V1ServiceAccount item) throws ApiException {
+		String name = getNameFromServiceAccount(item);
+		String namespace = item.getMetadata().getNamespace();
+		String secretName = item.getSecrets().get(0).getName();
+		V1Secret secret = getSecret(namespace, secretName, api);
+		return new User(name, 
+				getTokenFromSecret(secret), 
+				getCAFromSecret(secret), 
+				//getKubeconfigFromServiceAccount(item, api)
+				null
+				);
+	}
+
+	private String getCAFromSecret(V1Secret secret) {
+		return new String(secret.getData().get("ca.crt") );
+	}
+
+	private String getTokenFromSecret(V1Secret secret) {
+		return new String(secret.getData().get("token") );
+	}
+
+	private V1Secret getSecret(String namespace, String name, CoreV1Api api) throws ApiException {
+		V1Secret secret = api.readNamespacedSecret(name, namespace, null, true, true);
+		return secret;
+	}
+
+	private String getNameFromServiceAccount(V1ServiceAccount item) {
+		return item.getMetadata().getName();
 	}
 
 	public User[] read(String string) {
